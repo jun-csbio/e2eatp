727b6ac8e6db49b87c8d95c909f7be4b3ff16b76
jun-csbio <33274691+jun-csbio@users.noreply.github.com>
2023-07-11 20:02:45 +0800

    Create download.sh

---
+++ esm2m/download.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+wget -c https://dl.fbaipublicfiles.com/fair-esm/models/esm2_t33_650M_UR50D.pt -O esm2_t33_650M_UR50D.pt
+wget -c https://dl.fbaipublicfiles.com/fair-esm/regression/esm2_t33_650M_UR50D-contact-regression.pt -O esm2_t33_650M_UR50D-contact-regression.pt
+
+
