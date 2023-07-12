f39c78c70ebee675cb78f46e347c086aecfe83d7
jun-csbio <33274691+jun-csbio@users.noreply.github.com>
2023-07-12 14:18:20 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,3 +3,12 @@
 cd esm2m
 chmod 777 ./download.sh
 bash ./download.sh
+
+cd ..
+java -jar FileUnion.jar ./uniprot/human/ ./uniprot_human_2023_07_10.tar.gz
+rm -rf ./uniprot/human
+mv uniprot_human_2023_07_10.tar.gz ./uniprot/
+
+cd uniprot
+tar -zxvf uniprot_human_2023_07_10.tar.gz
+rm -f uniprot_human_2023_07_10.tar.gz
